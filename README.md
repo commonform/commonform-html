90ebfa6fd0e5a3e042f16d5e8770a0f855d4601c
Kyle Mitchell <kyle@kemitchell.com>
2015-08-24 15:15:14 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,81 @@
+```javascript
+var html = require('commonform-html')
+```
+
+Call the exported function with a Common Form, receive a string of HTML:
+
+```javascript
+var assert = require('assert')
+
+assert.equal(
+  html({ content: [ 'Just a test' ] }),
+  '<div class="article"><p><span>Just a test</span></p></div>')
+
+assert.equal(
+  html({
+    content: [
+      { heading: 'A',
+        form: { content: [ 'This is A' ] } },
+      { heading: 'B',
+        form: { content: [ 'This is B' ] } } ] }),
+  [ '<div class="article">',
+      '<div class="section">',
+        '<h1>A</h1>',
+        '<p><span>This is A</span></p>',
+      '</div>',
+      '<div class="section">',
+        '<h1>B</h1>',
+        '<p><span>This is B</span></p>',
+      '</div>',
+    '</div>' ]
+    .join(''))
+
+```
+
+You can also pass an `Object` map of fill-in-the-blank values:
+
+```javascript
+assert.equal(
+  html(
+    { content: [ { blank: 'name' } ] },
+    { name: 'Joe' }),
+  '<div class="article"><p><span class="blank">Joe</span></p></div>')
+```
+
+A final argument of `{ html5: true }` specifies HTML5 output:
+
+```javascript
+assert.equal(
+  html({ content: [ 'Just a test' ] }, { }, { html5: true }),
+  '<article><p><span>Just a test</span></p></article>')
+
+assert.equal(
+  html({
+    content: [
+      'First text defines a ',
+      { definition: 'Term' },
+      { heading: 'A',
+        form: { content: [ 'This is A' ] } },
+      'Middle text uses a ',
+      { use: 'Term' },
+      { heading: 'B',
+        form: { content: [ 'This is B' ] } },
+      'Last text references ',
+      { reference: 'Elsewhere' }] },
+    { },
+    { html5: true }),
+  [ '<article>',
+      '<p><span>First text defines a </span><dfn>Term</dfn></p>',
+      '<section>',
+        '<h1>A</h1>',
+        '<p><span>This is A</span></p>',
+      '</section>',
+      '<p><span>Middle text uses a </span><span class="term">Term</span></p>',
+      '<section>',
+        '<h1>B</h1>',
+        '<p><span>This is B</span></p>',
+      '</section>',
+      '<p><span>Last text references </span><span class="reference">Elsewhere</span></p>',
+    '</article>' ]
+    .join(''))
+```
---
+++ index.js
@@ -0,0 +1,80 @@
+var escape = require('escape-html')
+var predicate = require('commonform-predicate')
+var group = require('commonform-group-series')
+
+function renderParagraph(paragraph, blanks, html5) {
+  return  (
+    '<p>' +
+    paragraph.content
+      .map(function(element) {
+        if (predicate.text(element)) {
+          return (
+            '<span>' +
+            escape(element) +
+            '</span>' ) }
+        else if (predicate.use(element)) {
+          return (
+            '<span class="term">' +
+            escape(element.use) +
+            '</span>' ) }
+        else if (predicate.definition(element)) {
+          return (
+            ( html5 ? '<dfn>' : '<span class="definition">' ) +
+            escape(element.definition) +
+            ( html5 ? '</dfn>' : '</span>' ) ) }
+        else if (predicate.blank(element)) {
+          return (
+            '<span class="blank">' +
+            ( element.blank in blanks ?
+              escape(blanks[element.blank]) :
+              escape(element.blank) ) +
+            '</span>' ) }
+        else if (predicate.reference(element)) {
+          return (
+            '<span class="reference">' +
+            escape(element.reference) +
+            '</span>' ) } })
+        .join('') +
+    '</p>' ) }
+
+function heading(depth, text) {
+  if (depth <= 6) {
+    return (
+      '<h' + depth + '>' +
+      escape(text) +
+      '</h' + depth + '>' ) }
+  else {
+    return (
+      '<span class="h' + tag + '">' +
+      escape(text) +
+      '</span>' ) } }
+
+function renderSeries(depth, series, blanks, html5) {
+  return series.content
+    .map(function(child) {
+      return (
+        ( html5 ? '<section>' : '<div class="section">' ) +
+        ( 'heading' in child ? heading(depth, child.heading) : '' ) +
+        renderForm(depth, child.form, blanks, html5) +
+        ( html5 ? '</section>' : '</div>' ) ) })
+      .join('') }
+
+function renderForm(depth, form, blanks, html5) {
+  return group(form)
+    .map(function(group) {
+      return (
+        group.type === 'series' ?
+          renderSeries(( depth + 1 ), group, blanks, html5) :
+          renderParagraph(group, blanks, html5) ) })
+    .join('') }
+
+module.exports = function commonformHTML(form, blanks, options) {
+  if (!blanks) {
+    blanks = { } }
+  if (!options) {
+    options = { } }
+  var html5 = ('html5' in options && options.html5 === true)
+  return (
+    ( html5 ? '<article>' : '<div class="article">' ) +
+    renderForm(0, form, blanks, html5) +
+    ( html5 ? '</article>' : '</div>' ) ) }
