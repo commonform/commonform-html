d49d377d95acb001755d61bda5fbc2f6c0e7218d
Kyle E. Mitchell <kyle@kemitchell.com>
2016-08-21 14:04:15 -0700

    Use JavaScript Standard Style

--- README.md
+++ README.md
@@ -14,11 +14,18 @@ assert.equal(
 assert.equal(
   html({
     content: [
-      { heading: 'A',
-        form: { content: [ 'This is A' ] } },
-      { heading: 'B',
-        form: { content: [ 'This is B' ] } } ] }),
-  [ '<div class="article">',
+      {
+        heading: 'A',
+        form: {content: ['This is A']}
+      },
+      {
+        heading: 'B',
+        form: {content: ['This is B']}
+      }
+    ]
+  }),
+  [
+    '<div class="article">',
       '<div class="section">',
         '<h1>A</h1>',
         '<p>This is A</p>',
@@ -27,9 +34,10 @@ assert.equal(
         '<h1>B</h1>',
         '<p>This is B</p>',
       '</div>',
-    '</div>' ]
-    .join(''))
-
+    '</div>'
+  ]
+  .join('')
+)
 ```
 
 You can also pass an `Array` of fill-in-the-blank values:
@@ -37,42 +45,61 @@ You can also pass an `Array` of fill-in-the-blank values:
 ```javascript
 assert.equal(
   html(
-    { content: [
-        { blank: '' },
-        { form: { content: [ 'Another ', { blank: '' } ] } } ] },
-    [ { blank: [ 'content', 0 ], value: 'Joe' },
-      { blank: [ 'content', 1, 'form', 'content', 1 ], value: 'Bob' } ]),
-  ( '<div class="article">' +
+    {
+      content: [
+        {blank: ''},
+        {form: {content: ['Another ', {blank: ''}]}}
+      ]
+    },
+    [
+      {blank: ['content', 0], value: 'Joe'},
+      {blank: ['content', 1, 'form', 'content', 1], value: 'Bob'}
+    ]
+  ),
+  (
+    '<div class="article">' +
       '<p><span class="blank">Joe</span></p>' +
       '<div class="section">' +
         '<p>Another <span class="blank">Bob</span></p>' +
       '</div>' +
-    '</div>' ))
+    '</div>'
+  )
+)
 ```
 
 A final argument of `{ html5: true }` specifies HTML5 output:
 
 ```javascript
 assert.equal(
-  html({ content: [ 'Just a test' ] }, [ ], { html5: true }),
-  '<article><p>Just a test</p></article>')
+  html({content: ['Just a test']}, [], {html5: true}),
+  '<article><p>Just a test</p></article>'
+)
 
 assert.equal(
-  html({
-    content: [
-      'First text defines a ',
-      { definition: 'Term' },
-      { heading: 'A',
-        form: { content: [ 'This is A' ] } },
-      'Middle text uses a ',
-      { use: 'Term' },
-      { heading: 'B',
-        form: { content: [ 'This is B' ] } },
-      'Last text references ',
-      { reference: 'Elsewhere' }] },
-    [ ],
-    { html5: true }),
-  [ '<article>', // not <div class="article">
+  html(
+    {
+      content: [
+        'First text defines a ',
+        {definition: 'Term'},
+        {
+          heading: 'A',
+          form: {content: ['This is A']}
+        },
+        'Middle text uses a ',
+        {use: 'Term'},
+        {
+          heading: 'B',
+          form: {content: ['This is B']}
+        },
+        'Last text references ',
+        {reference: 'Elsewhere'}
+      ]
+    },
+    [],
+    {html5: true}
+  ),
+  [
+    '<article>', // not <div class="article">
       '<p>',
         'First text defines a ',
         '<dfn>Term</dfn>', // not <span class="definition">
@@ -93,8 +120,10 @@ assert.equal(
         'Last text references ',
         '<span class="reference">Elsewhere</span>',
       '</p>',
-    '</article>' ]
-    .join(''))
+    '</article>'
+  ]
+  .join('')
+)
 ```
 
 You can also set a title:
@@ -102,14 +131,18 @@ You can also set a title:
 ```javascript
 assert.equal(
   html(
-    { content: [ 'Hello, ', { blank: '' } ] },
-    [ { blank: [ 'content', 1 ], value: 'Joe' } ],
-    { title: 'Welcome' }),
-  [ '<div class="article">',
+    {content: ['Hello, ', {blank: ''}]},
+    [{blank: ['content', 1], value: 'Joe'}],
+    {title: 'Welcome'}
+  ),
+  [
+    '<div class="article">',
       '<h1>Welcome</h1>',
       '<p>',
         'Hello, <span class="blank">Joe</span>',
       '</p>',
-    '</div>' ]
-    .join('') )
+    '</div>'
+  ]
+  .join('')
+)
 ```
--- index.js
+++ index.js
@@ -3,107 +3,132 @@ var equal = require('deep-equal')
 var group = require('commonform-group-series')
 var predicate = require('commonform-predicate')
 
-function renderParagraph(paragraph, offset, path, blanks, html5) {
-  return  (
+function renderParagraph (paragraph, offset, path, blanks, html5) {
+  return (
     '<p>' +
     paragraph.content
-      .map(function(element, index) {
-        if (predicate.text(element)) {
-          return escape(element) }
-        else if (predicate.use(element)) {
-          return (
-            '<span class="term">' +
-            escape(element.use) +
-            '</span>' ) }
-        else if (predicate.definition(element)) {
-          return (
-            ( html5 ? '<dfn>' : '<span class="definition">' ) +
-            escape(element.definition) +
-            ( html5 ? '</dfn>' : '</span>' ) ) }
-        else if (predicate.blank(element)) {
-          var elementPath = path.concat('content', ( offset + index ))
-          var value = matchingValue(elementPath, blanks)
-          return (
-            '<span class="blank">' +
-            ( value ?
-              escape(value) :
-              escape('[•]') ) +
-            '</span>' ) }
-        else if (predicate.reference(element)) {
-          return (
-            '<span class="reference">' +
-            escape(element.reference) +
-            '</span>' ) } })
-        .join('') +
-    '</p>' ) }
+    .map(function (element, index) {
+      if (predicate.text(element)) {
+        return escape(element)
+      } else if (predicate.use(element)) {
+        return (
+          '<span class="term">' +
+          escape(element.use) +
+          '</span>'
+        )
+      } else if (predicate.definition(element)) {
+        return (
+          (html5 ? '<dfn>' : '<span class="definition">') +
+          escape(element.definition) +
+          (html5 ? '</dfn>' : '</span>')
+        )
+      } else if (predicate.blank(element)) {
+        var elementPath = path.concat('content', offset + index)
+        var value = matchingValue(elementPath, blanks)
+        return (
+          '<span class="blank">' +
+          (value ? escape(value) : escape('[•]')) +
+          '</span>'
+        )
+      } else if (predicate.reference(element)) {
+        return (
+          '<span class="reference">' +
+          escape(element.reference) +
+          '</span>'
+        )
+      }
+    })
+    .join('') +
+    '</p>'
+  )
+}
 
-function matchingValue(path, blanks) {
+function matchingValue (path, blanks) {
   var length = blanks.length
   for (var index = 0; index < length; index++) {
     var blank = blanks[index]
     if (equal(blank.blank, path)) {
-      return blank.value } } }
+      return blank.value
+    }
+  }
+}
 
-function heading(depth, text) {
+function heading (depth, text) {
   if (depth <= 6) {
     return (
       '<h' + depth + '>' +
       escape(text) +
-      '</h' + depth + '>' ) }
-  else {
+      '</h' + depth + '>'
+    )
+  } else {
     return (
       '<span class="h' + depth + '">' +
       escape(text) +
-      '</span>' ) } }
+      '</span>'
+    )
+  }
+}
 
-function renderSeries(depth, offset, path, series, blanks, html5) {
+function renderSeries (depth, offset, path, series, blanks, html5) {
   return series.content
-    .map(function(child, index) {
-      return (
-        ( html5 ?
-          ( child.form.conspicuous ?
-              '<section class="conspicuous">' :
-              '<section>' ) :
-          ( child.form.conspicuous ?
-              '<div class="section conspicuous">' :
-              '<div class="section">' ) ) +
-        ( 'heading' in child ? heading(depth, child.heading) : '' ) +
-        renderForm(
-          depth,
-          path.concat('content', ( offset + index ), 'form'),
-          child.form,
-          blanks, html5) +
-        ( html5 ? '</section>' : '</div>' ) ) })
-      .join('') }
+  .map(function (child, index) {
+    return (
+      (
+        html5
+        ? child.form.conspicuous
+          ? '<section class="conspicuous">'
+          : '<section>'
+        : child.form.conspicuous
+          ? '<div class="section conspicuous">'
+          : '<div class="section">'
+      ) +
+      ('heading' in child ? heading(depth, child.heading) : '') +
+      renderForm(
+        depth,
+        path.concat('content', offset + index, 'form'),
+        child.form,
+        blanks, html5
+      ) +
+      (html5 ? '</section>' : '</div>')
+    )
+  })
+  .join('')
+}
 
-function renderForm(depth, path, form, blanks, html5) {
+function renderForm (depth, path, form, blanks, html5) {
   var offset = 0
   return group(form)
-    .map(function(group) {
-      var returned = (
-        group.type === 'series' ?
-          renderSeries(( depth + 1 ), offset, path, group, blanks, html5) :
-          renderParagraph(group, offset, path, blanks, html5) )
-      offset += group.content.length
-      return returned })
-    .join('') }
+  .map(function (group) {
+    var returned = group.type === 'series'
+    ? renderSeries(depth + 1, offset, path, group, blanks, html5)
+    : renderParagraph(group, offset, path, blanks, html5)
+    offset += group.content.length
+    return returned
+  })
+  .join('')
+}
 
-module.exports = function commonformHTML(form, blanks, options) {
+module.exports = function commonformHTML (form, blanks, options) {
   if (!blanks) {
-    blanks = [ ] }
+    blanks = []
+  }
   if (!options) {
-    options = { } }
-  var html5 = ( 'html5' in options && options.html5 === true )
-  var title = ( 'title' in options ?
-    options.title : false )
+    options = {}
+  }
+  var html5 = 'html5' in options && options.html5 === true
+  var title = 'title' in options ? options.title : false
   return (
-    ( html5 ?
-      ( form.conspicuous ?
-          '<article class="conspicuous">' :
-          '<article>' ) :
-      ( form.conspicuous ?
-          '<div class="article conspicuous">' :
-          '<div class="article">' ) ) +
-    ( title ? ( '<h1>' + escape(title) + '</h1>' ) : '' ) +
-    renderForm(( title ? 1 : 0 ), [ ], form, blanks, html5) +
-    ( html5 ? '</article>' : '</div>' ) ) }
+    (
+      html5
+      ? form.conspicuous
+        ? '<article class="conspicuous">'
+        : '<article>'
+      : form.conspicuous
+        ? '<div class="article conspicuous">'
+        : '<div class="article">'
+    ) +
+    (title ? ('<h1>' + escape(title) + '</h1>') : '') +
+    renderForm((title ? 1 : 0), [], form, blanks, html5) +
+    (html5 ? '</article>' : '</div>')
+  )
+}
